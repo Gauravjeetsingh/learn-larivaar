ea0e7f7303b2ea15256bcdd4758fcb7a9f03ab52
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2024-07-25 12:56:40 -0400

    fix: resolve clipping issue in about page - fix app icon issue for android builds

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -10,6 +10,7 @@ import {
 import {elementStyles} from '../../styles';
 import {useTheme} from '@react-navigation/native';
 import {ScrollView} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const About = ({navigation}: any): JSX.Element => {
   const isDarkMode = useTheme().dark;
@@ -23,42 +24,44 @@ const About = ({navigation}: any): JSX.Element => {
   };
   return (
     <ScrollView>
-      <Text style={themeStyles.aboutText}>Created by</Text>
-      <Image
-        style={themeStyles.logo}
-        source={
-          isDarkMode
-            ? require('../../assets/images/khalis-logo-dark.png')
-            : require('../../assets/images/khalis-logo.png')
-        }
-      />
-      <Text style={themeStyles.aboutText}>
-        We welcome your comments, suggestions and corrections! For more
-        information, visit us at
-        <TouchableOpacity onPress={handlePressKhalis}>
-          <Text style={themeStyles.linkText}>Khalisfoundation.org</Text>
-        </TouchableOpacity>
-      </Text>
-      <Text style={themeStyles.aboutText}>
-        Please respectfully cover your head and remove your shoes when using
-        this app.
-      </Text>
-      <Text style={themeStyles.aboutText}>
-        Learn Larivaar utilizes{' '}
-        <Text onPress={handlePressBaniDB} style={themeStyles.linkText}>
-          BaniDB
-        </Text>
-        - the open source Gurbani Database and API used in many gurbani
-        applications
-      </Text>
-      <View style={elementStyles(currentTheme).button}>
-        <Button
-          title="Go back"
-          onPress={() => {
-            navigation.goBack();
-          }}
+      <SafeAreaView>
+        <Text style={themeStyles.aboutText}>Created by</Text>
+        <Image
+          style={themeStyles.logo}
+          source={
+            isDarkMode
+              ? require('../../assets/images/khalis-logo-dark.png')
+              : require('../../assets/images/khalis-logo.png')
+          }
         />
-      </View>
+        <Text style={themeStyles.aboutText}>
+          We welcome your comments, suggestions and corrections! For more
+          information, visit us at
+          <TouchableOpacity onPress={handlePressKhalis}>
+            <Text style={themeStyles.linkText}>Khalisfoundation.org</Text>
+          </TouchableOpacity>
+        </Text>
+        <Text style={themeStyles.aboutText}>
+          Please respectfully cover your head and remove your shoes when using
+          this app.
+        </Text>
+        <Text style={themeStyles.aboutText}>
+          Learn Larivaar utilizes{' '}
+          <Text onPress={handlePressBaniDB} style={themeStyles.linkText}>
+            BaniDB
+          </Text>
+          - the open source Gurbani Database and API used in many gurbani
+          applications
+        </Text>
+        <View style={elementStyles(currentTheme).button}>
+          <Button
+            title="Go back"
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+        </View>
+      </SafeAreaView>
     </ScrollView>
   );
 };
